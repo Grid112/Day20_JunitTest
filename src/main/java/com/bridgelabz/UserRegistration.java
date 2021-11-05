bf95099e39608ca1d4ee32cbc8cb72fc9d7b5cce
Grid112 <sumitware1999@gmail.com>
2021-11-05 20:43:50 +0530

    Added: User Registration Email Id Validation Test

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -9,16 +9,27 @@ public class UserRegistration {
     Pattern pattern;
     Matcher matcher;
 
+    // Method for First Name Validation
     public boolean firstNameValidation (String firstName){
         String regexFirstName = "^[A-Z]{1}[a-z]{2,}$";
         pattern = Pattern.compile(regexFirstName);
         matcher = pattern.matcher(firstName);
         return matcher.matches();
     }
+
+    // Method for Last Name Validation
     public boolean lastNameValidation (String lastName){
         String regexLastName = "^[A-Z]{1}[a-z]{2,}$";
         pattern = Pattern.compile(regexLastName);
         matcher = pattern.matcher(lastName);
+        return matcher.matches();
+    }
+
+    // Method for Email ID Validation
+    public boolean emailIdValidation (String emailId){
+        String regexEmailId = "^[A-Z a-z 1-9]+([.][A-Z a-z 1-9]+)*@[A-Z a-z]{3,5}+.[a-z]{2,3}+([.][a-z]{2})*$";
+        pattern = Pattern.compile(regexEmailId);
+        matcher = pattern.matcher(emailId);
         return matcher.matches();
     }
 }
--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -10,16 +10,27 @@ public class UserRegistrationTest {
     UserRegistration Validate = new UserRegistration();
     Scanner Sc = new Scanner(System.in);
 
+    // Test for First Name Validation
     @Test
     public void givenFirstName_WhenProper_ShouldReturnTrue(){
         boolean result = Validate.firstNameValidation("Sumit");
             Assert.assertEquals(true, result);
             System.out.println("\n First Name Is Successfully Validated");
     }
+
+    // Test for Last Name Validation
     @Test
     public void givenLastName_WhenProper_ShouldReturnTrue(){
         boolean result = Validate.firstNameValidation("Ware");
         Assert.assertEquals(true, result);
         System.out.println(" Last Name Is Successfully Validated");
+    }
+
+    // Test for Email ID Validation
+    @Test
+    public void givenEmailId_WhenProper_ShouldReturnTrue(){
+        boolean result = Validate.emailIdValidation("Sumitware1999@gmail.com");
+        Assert.assertEquals(true, result);
+        System.out.println(" Email ID is Successfully Validated ");
     }
 }
